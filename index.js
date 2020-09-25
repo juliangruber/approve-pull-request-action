b9dafcf80f5ff165bc225ff17914aa0c9cdec292
Zeke Sikelianos <zeke@sikelianos.com>
2020-09-24 20:24:53 -0700

    feat: approve once

--- index.js
+++ index.js
@@ -6,9 +6,18 @@ const { GitHub, context } = require('@actions/github')
 const main = async () => {
   const token = core.getInput('github-token')
   const number = core.getInput('number')
-
   const octokit = new GitHub(token)
 
+  // Check for existing approvals before approving a second time
+  const { data: reviews } = await octokit.pulls.listReviews({
+    ...context.repo,
+    pull_number: number
+  })
+  const user = await octokit.users.getAuthenticated()
+  const approved = reviews.some(review => review.state === 'APPROVED' && review.user.login === user.login)
+  if (approved) return
+
+  // Approve
   await octokit.pulls.createReview({
     ...context.repo,
     pull_number: number,
