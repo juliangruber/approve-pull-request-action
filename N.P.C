60ad212b1a8b4370a60e5930f69e09d7876d6b2f
ZACHRY T WOODzachryiixixiiwood@gmail.com <109656750+zakwarlord7@users.noreply.github.com>
2022-12-27 17:24:21 -0600

    Create N.P.C

---
+++ N.P.C
@@ -0,0 +1,41 @@
+**''!'#'/'#' :' ''#'#':''' '':''Run'''
+'BEGIN :'
+'GLOW4 :'
+  - ''-'' '#'Name':'' 'O'P'E'N.json/pkg'.js':''
+  -'' 'with'' ':'Gulp'.xml'' ':''Run''
+'Run'' ':'Run::/Start::/starts::/::BEGIN:-on'::/:GLOW4::''
+  :GLOW$::'' ':'' '.'Docx'' ':'' 'Runs-on'' ':'Run''
+'Run'' ':'' 'Run-on'' 'Run'' :
+-  push:
+- 'branches: '- '[ '"trunk" ']'
+-  pull_request:
+- 'branches ': '- '[ 'mainbranch ']''
+-jobs:
+-  build:
+-    runs-on: ubuntu-latest
+-    strategy:
+-      matrix:
+-        buster'@v'-'"14x:; :16x:; :18x:; ::''
+#Job :use :"'-'"''    'Step'' :
+'-'' 'diff --git a/.github/ISSUE_TEMPLATE/production-config-change.md b/.github/ISSUE_TEMPLATE/production-config-change.md
+index 5096b3c64e53..38fabd74abb5 100644
+--- a/.github/ISSUE_TEMPLATE/production-config-change.md
++++ b/.github/ISSUE_TEMPLATE/production-config-change.md
+@@ -5,7 +5,7 @@ title: ''
+ labels: engineering
+ assignees: ''
+ ---
+-
++<li>zachryiixixiiwood@gmail.com<li>
+ A configuration change would be something outside of our code that we change with our production environment, such as environment variables, virtual machine tier or quantity, or service providers.
+
+ - _Primary person_:    
+'-'' 'use'' ':'#'Action'.js'/'#'Checks'-out/repositories/dispatch'@v'-'"3'"''
+-    - name: Use Node.js ${{ matrix.node-version }}
+-      uses: actions/setup-node@v3
+-      with:
+-        node-version: ${{ matrix.node-version }}
+-    - name: Build
+-      run: |
+-        npm install
+-        Gulp.xml**
